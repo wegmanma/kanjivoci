be7d72157bcad30fca5e5821a36f2542211d4997
Marcel Wegmann <marcel.wegmann@outlook.com>
2025-01-19 11:27:07 +0100

    Prepared a dummy struct with entries in datawriter
    Signed-off-by: Marcel Wegmann <marcel.wegmann@outlook.com>

--- kanjivoci_datawriter/src/main.rs
+++ kanjivoci_datawriter/src/main.rs
@@ -1,4 +1,31 @@
 use kanjivoci_lib;
 fn main() {
-    let carddecks = kanjivoci_lib::Carddecks::new();
+    let mut carddeck = kanjivoci_lib::Carddecks::new();
+
+    let mut on_readings = Vec::new();
+    on_readings.push("ジン".to_string());
+    on_readings.push("ニン".to_string());
+    let mut kun_readings = Vec::new();
+    kun_readings.push("ひと".to_string());
+    kun_readings.push("びと".to_string());
+    kun_readings.push("り".to_string());
+    kun_readings.push("と".to_string());
+    carddeck.add_kanjicard("人", &on_readings, &kun_readings, 5, "person");
+
+    let mut readings = Vec::new();
+    readings.push("こい".to_string());
+    readings.push("びと".to_string());
+
+    carddeck.add_vocabcard("恋人", &readings, "loved one");
+
+    let mut readings = Vec::new();
+    readings.push("ゆう".to_string());
+    readings.push("じん".to_string());
+
+    carddeck.add_vocabcard("友人", &readings, "friend (formal)");
+
+    carddeck.vocab_kanji_align_score("人", -1.5);
+    carddeck.kanji_kanji_align_score("人", 1.2);
+    carddeck.sort_decks();
+    carddeck.print_decks();
 }
