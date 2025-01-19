5d82a07d02953f74f88924dbaae2a26d17fa055e
Marcel Wegmann <marcel.wegmann@outlook.com>
2025-01-19 11:11:11 +0100

    Overhaul to lib, transformed spelling to vector

--- kanjivoci_datawriter/src/main.rs
+++ kanjivoci_datawriter/src/main.rs
@@ -1,6 +1,4 @@
 use kanjivoci_lib;
-use std::fs;
-use toml;
 fn main() {
     let carddecks = kanjivoci_lib::Carddecks::new();
 }
--- kanjivoci_lib/src/lib.rs
+++ kanjivoci_lib/src/lib.rs
@@ -1,24 +1,27 @@
 #[derive(Debug)]
-pub struct kanjivoci {
+pub struct Kanjicard {
     pub kanji: String,
     pub on_reading: Vec<String>,
     pub kun_reading: Vec<String>,
     pub meaning: String,
+    pub level: i8, // N5 -> 5 till N1 -> 1. If 0: Not set yet
     pub score: f32, // the higher, the better known
 }
 
-impl kanjivoci {
+impl Kanjicard {
     pub fn new(
         kanji: &str,
         on_reading: &Vec<String>,
         kun_reading: &Vec<String>,
+        level: i8,
         meaning: &str,
     ) -> Self {
-        kanjivoci {
+        Kanjicard {
             kanji: kanji.to_string(),
             on_reading: on_reading.clone(),
             kun_reading: kun_reading.clone(),
             meaning: meaning.to_string(),
+            level: level,
             score: 10.0, // Default score
         }
     }
@@ -31,12 +34,12 @@ impl kanjivoci {
     }
 }
 
-impl std::fmt::Display for kanjivoci {
+impl std::fmt::Display for Kanjicard {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(
             f,
-            "Kanji: {}, On Reading: {:?}, Kun Reading: {:?}, Meaning: {}, Score: {}",
-            self.kanji, self.on_reading, self.kun_reading, self.meaning, self.score
+            "Kanji: {}, On Reading: {:?}, Kun Reading: {:?}, Meaning: {}, Level: {}, Score: {}",
+            self.kanji, self.on_reading, self.kun_reading, self.meaning, self.level, self.score
         )
     }
 }
@@ -44,16 +47,16 @@ impl std::fmt::Display for kanjivoci {
 #[derive(Debug)]
 pub struct Vocabcard {
     pub kanji: String,
-    pub spelling: String,
+    pub spelling: Vec<String>,
     pub meaning: String,
     pub score: f32, // the higher, the better known
 }
 
 impl Vocabcard {
-    pub fn new(kanji: &str, spelling: &str, meaning: &str) -> Self {
+    pub fn new(kanji: &str, spelling: &Vec<String>, meaning: &str) -> Self {
         Vocabcard {
             kanji: kanji.to_string(),
-            spelling: spelling.to_string(),
+            spelling: spelling.clone(),
             meaning: meaning.to_string(),
             score: 10.0, // Default score
         }
@@ -71,7 +74,7 @@ impl std::fmt::Display for Vocabcard {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(
             f,
-            "Kanji: {}, Spelling: {}, Meaning: {}, Score: {}",
+            "Kanji: {}, Spelling: {:?}, Meaning: {}, Score: {}",
             self.kanji, self.spelling, self.meaning, self.score
         )
     }
@@ -79,36 +82,37 @@ impl std::fmt::Display for Vocabcard {
 
 #[derive(Debug)]
 pub struct Carddecks {
-    kanjivocis: Vec<kanjivoci>,
+    kanjicards: Vec<Kanjicard>,
     vocabcards: Vec<Vocabcard>,
 }
 
 impl Carddecks {
     pub fn new() -> Self {
         Carddecks {
-            kanjivocis: Vec::new(),
+            kanjicards: Vec::new(),
             vocabcards: Vec::new(),
         }
     }
-    pub fn add_kanjivoci(
+    pub fn add_kanjicard(
         &mut self,
         kanji: &str,
         on_reading: &Vec<String>,
         kun_reading: &Vec<String>,
+        level: i8,
         meaning: &str,
     ) {
-        self.kanjivocis
-            .push(kanjivoci::new(kanji, on_reading, kun_reading, meaning));
+        self.kanjicards
+            .push(Kanjicard::new(kanji, on_reading, kun_reading, level, meaning));
     }
 
-    pub fn add_vocabcard(&mut self, kanji: &str, spelling: &str, meaning: &str) {
+    pub fn add_vocabcard(&mut self, kanji: &str, spelling: &Vec<String> , meaning: &str) {
         self.vocabcards
             .push(Vocabcard::new(kanji, spelling, meaning));
     }
 
     pub fn sort_decks(&mut self) {
         // Sort the flashcards by score (ascending, so lower scores come first)
-        self.kanjivocis.sort_by(|a, b| {
+        self.kanjicards.sort_by(|a, b| {
             a.score
                 .partial_cmp(&b.score)
                 .unwrap_or(std::cmp::Ordering::Equal)
@@ -128,14 +132,14 @@ impl Carddecks {
     }
 
     pub fn kanji_kanji_align_score(&mut self, kanji: &str, delta: f32) {
-        if let Some(card) = self.kanjivocis.iter_mut().find(|x| x.kanji == kanji) {
+        if let Some(card) = self.kanjicards.iter_mut().find(|x| x.kanji == kanji) {
             card.update_score(delta);
         }
     }
 
     pub fn print_decks(self) {
         println!("kanjivocis");
-        for card in self.kanjivocis.iter() {
+        for card in self.kanjicards.iter() {
             println!("{}", card);
         }
         println!("Vocabcards");
